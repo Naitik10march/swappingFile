a29d6a450e3fd457cf85a3c8797e56f362fce4ef
Naitik10march <79587004+Naitik10march@users.noreply.github.com>
2022-04-17 20:12:05 +0530

    Add files via upload

---
+++ Swappingfile.py
@@ -0,0 +1,11 @@
+myfile = "file1.txt"
+print(myfile)
+
+data_a = "1 This is the text from file 2"
+print(data_a)
+
+myfile2 = "file2.txt"
+print(myfile2)
+
+data_b = "1 This is the text from file 1"
+print(data_b)
